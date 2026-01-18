3f7ae2a98d375795dd24b40480085b9a50b146c3
shagun-sainii <shagunsaini0387@gmail.com>
2026-01-18 18:35:42 +0530

    Update guess_game.py

--- guess_game.py
+++ guess_game.py
@@ -5,12 +5,16 @@ computer_choice = random.randint(1,10)
 print("you got three chances to win!!")
 i = 3
 while i >= 1 :
-    user_input = int(input("enter your number "))
+    try:
+     user_input = int(input("enter your number "))
+    except ValueError:
+        print("enter a valid number")
     if (user_input == computer_choice):
         print("you won!!")
         break 
     else :
-        print("try again !! chances left =" , i-1  )
+        if (i>1) : 
+           print("try again !! chances left =" , i-1  )
     i -= 1
 if(i<1) :
     print(" oops , chances are over !! , the number was ", computer_choice)
